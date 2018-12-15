d75cfe70db7bc370c618dd00201fb0a4b8804631
diana514 <45902639+diana514@users.noreply.github.com>
2018-12-15 19:34:20 +0100

    adee

---
+++ first_exercise.py
@@ -0,0 +1,42 @@
+#import math.pi
+#pi=3.14
+
+def media (x,y):
+	m=(x+y)
+	return int(m)/2
+
+#x=int(input())
+#y=int(input())
+
+#print(x+y)
+#print (media (x,y))
+
+
+
+#raggio=float(input("inserisci raggio"))
+#area=raggio * raggio * pi
+#circonferenza=raggio*2*pi
+#print("area=",area)
+#print("circonferenza=",circonferenza)
+
+#c1=int(input("c1"))
+#c2=int(input("c2"))
+
+
+
+#import math
+
+#math.sqrt
+
+#ipo=math.sqrt(c1**2+c2**2)
+#print("ipo=",ipo)
+
+galline=int(input("n galline"))
+conigli=int(input('n conigli'))
+GG=2 # gambegalline
+GC=4 
+gambe=GG*galline+GC*conigli
+print("gambe=",gambe)
+
+
+
